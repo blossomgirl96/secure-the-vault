615109ced8428cd23064587fa9cbfe0210e6778b
Meera Vinod <meerarevathy.99@gmail.com>
2026-02-11 20:58:20 -0500

    Adding stuff

---
+++ index.js
@@ -0,0 +1,25 @@
+let code1;
+code1 = 7 + 3;
+console.log(code1);
+
+
+let code2;
+code2 = 90 / 2 - 5;
+console.log(code2);
+
+
+let code3;
+code3 = 509 / 2 - 215.5;
+console.log(code3);
+
+let code4;
+code4 = "The vault has been secured. The combination is:";
+console.log(code4)
+
+let codeA;
+codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA)
+
+let codeB;
+codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB)
